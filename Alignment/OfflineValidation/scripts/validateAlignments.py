56847711256843e5decab006aef90e0dbaae69ed
Federico Vazzoler <federico.vazzoler@cern.ch>
2023-05-08 17:37:27 +0200

    Add the MTS module to the validateAlignments.py script

--- Alignment/OfflineValidation/scripts/validateAlignments.py
+++ Alignment/OfflineValidation/scripts/validateAlignments.py
@@ -17,6 +17,7 @@ import Alignment.OfflineValidation.TkAlAllInOneTool.DMR as DMR
 import Alignment.OfflineValidation.TkAlAllInOneTool.PV as PV
 import Alignment.OfflineValidation.TkAlAllInOneTool.SplitV as SplitV
 import Alignment.OfflineValidation.TkAlAllInOneTool.JetHT as JetHT
+import Alignment.OfflineValidation.TkAlAllInOneTool.MTS as MTS
 
 ##############################################
 def parser():
@@ -258,6 +259,8 @@ def main():
         elif validation == "JetHT":
             jobs.extend(JetHT.JetHT(config, validationDir))
 
+        elif validation == "MTS":
+            jobs.extend(MTS.MTS(config, validationDir))
         else:
             raise Exception("Unknown validation method: {}".format(validation)) 
             
